f7de211fdc0a1ec05d83438c18373e23a8bcca5a
Uladzimir Hanchar <u.hanczar@gmail.com>
2020-03-15 22:49:19 +0300

    PA: Removing client to separate repository for deploying

--- client/src/util/CustomPopup.js
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-import { Popup } from 'semantic-ui-react';
-
-const CustomPopup = ({ content, children }) => <Popup inverted content={content} trigger={children} />;
-
-export default CustomPopup;
