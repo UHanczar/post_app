6c66b5a5794b54d53a0328f744330cd8cf5ef553
Uladzimir Hanchar <u.hanczar@gmail.com>
2020-03-15 22:35:08 +0300

    PA-13: Adding popups to comment, like and delete buttons

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const { MONGO_DB_KEY } = require('./config');
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 
+const PORT = process.env.PORT || 5000;
 const pubSub = new PubSub();
 
 const server = new ApolloServer({
@@ -14,5 +15,6 @@ const server = new ApolloServer({
 });
 
 mongoose.connect(MONGO_DB_KEY, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => server.listen({ port: 5000 }))
-  .then(res => console.log(`Server runs on http://localhost:${res.port}`));
+  .then(() => server.listen({ port: PORT }))
+  .then(res => console.log(`Server runs on http://localhost:${res.port}`))
+  .catch(error => console.error('Database connection error'));
